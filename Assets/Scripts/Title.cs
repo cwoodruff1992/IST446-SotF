1996119d3a74627f0902ee7cf7dcfc28d74cdb2b
Charles Woodruff <GeekSquad1992@gmail.com>
2016-04-24 02:19:06 -0400

    Started on title screen, 1 of 3 buttons shows, for some reason

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+    // Draw the GUI
+    void OnGUI() {
+        const int buttonWidth = 120;
+        const int buttonHeight = 60;
+
+        Rect startRect = new Rect(Screen.width / 3 - (buttonWidth / 2), (9 * Screen.height / 10) - (buttonHeight / 2), buttonWidth, buttonHeight);
+        Rect controlsRect = new Rect(Screen.width / 3 - (buttonWidth / 2), (((9 * Screen.height / 10) - (buttonHeight / 2)) + (buttonWidth * 2.5f)), buttonWidth, buttonHeight);
+        Rect creditsRect = new Rect(Screen.width / 3 - (buttonWidth / 2), (((9 * Screen.height / 10) - (buttonHeight / 2)) + (buttonWidth * 5)), buttonWidth, buttonHeight);
+
+        if (GUI.Button(startRect, "Play")) {
+            SceneManager.LoadScene("Level 1");
+        }
+
+        if (GUI.Button(controlsRect, "Controls")) {
+            SceneManager.LoadScene("Controls");
+        }
+
+        if (GUI.Button(creditsRect, "Credits")) {
+            SceneManager.LoadScene("Credits");
+        }
+    }
+}
